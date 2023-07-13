3aaa629b223e4811e39f6817d83f4a2d452c5245
Sophie Libkind <4992461+slibkind@users.noreply.github.com>
2023-07-12 17:09:24 -0700

    loop to allow multiple task detail pairs

--- find_fixed_points.py
+++ find_fixed_points.py
@@ -1,4 +1,3 @@
-#%%
 import torch
 import os
 import time
@@ -11,9 +10,18 @@ from utils.utils import get_model, get_analysis_path, get_fixed_point_path
 model_name = "delaygo_delayanti_255"
 
 # Define task details
-task_idx = [0, 0]
-period = ["stim", "delay"]
-stimulus = [1, 2]
+task_details = [
+    ([0, 0], ["fix", "stim"], [1, 1]),    # run through the delaygo task with stimulus 1
+    ([0, 0], ["stim", "delay"], [1, 1]),
+    ([0, 0], ["delay", "go"], [1, 1]),
+    ([0, 0], ["delay", "delay"], [1, 2]), # compare stim 1 and 2 in delaygo task
+    ([0, 0], ["stim", "delay"], [2, 2]),
+    ([0, 1], ["delay", "delay"], [1, 1]),  # compare delaygo vs. delayanti tasks
+    ([0, 1], ["delay", "delay"], [2, 2]),    
+    ([1, 1], ["stim", "delay"], [1, 1]),
+    ([1, 1], ["stim", "delay"], [2, 2])
+]
+
 
 # Interpolation steps
 n_interp = 20
@@ -42,32 +50,35 @@ num_samples = int(hidden_state_count * sample_proportion)  # Calculate the numbe
 indices = torch.randperm(hidden_state_count)[:num_samples]  
 sampled_hiddens = all_hiddens[indices]  # Select the sampled hidden points using the sampled indices
 
-# Generate task inputs
-input1 = get_input(task_idx[0], period[0], stimulus[0], tasks)
-input2 = get_input(task_idx[1], period[1], stimulus[1], tasks)
 
-for i in range(n_interp + 1):
+# Generate task inputs for each pair of details
+for task_idx, period, stimulus in task_details:
     
-    print(f"Interpolation step {i+1} of {n_interp+1}")
+    # Generate the task inputs
+    input1 = get_input(task_idx[0], period[0], stimulus[0], tasks)
+    input2 = get_input(task_idx[1], period[1], stimulus[1], tasks)
     
-    # Linearly interpolate between the inputs
-    interpolated_input = (n_interp - i)/n_interp * input1 + (i/n_interp) * input2
-
-    # Start timing
-    start_time = time.time()
+    for i in range(n_interp + 1):
+        
+        print(f"Task Details: {task_idx}, {period}, {stimulus}. Interpolation step {i+1} of {n_interp+1}")
+        
+        # Linearly interpolate between the inputs
+        interpolated_input = (n_interp - i)/n_interp * input1 + (i/n_interp) * input2
     
-    # Find the fixed points for the interpolated_input
-    fixed_points = minimize_speed(rnn, interpolated_input, sampled_hiddens, learning_rate,
-                                  speed_threshold, verbose=True, method="second")
+        # Start timing
+        start_time = time.time()
+        
+        # Find the fixed points for the interpolated_input
+        fixed_points = minimize_speed(rnn, interpolated_input, sampled_hiddens, learning_rate,
+                                      speed_threshold, verbose=True, method="second")
+    
+        # End timing and print the execution time
+        end_time = time.time()
+        print(f"Finding fixed points took {end_time - start_time} seconds.")
+    
+        # Save the fixed points
+        fixed_point_path = get_fixed_point_path(model_name, interpolated_input) 
+        torch.save(fixed_points, fixed_point_path)
+    
+        print(f"Fixed points saved at {fixed_point_path}\n")
 
-    # End timing and print the execution time
-    end_time = time.time()
-    print(f"Finding fixed points took {end_time - start_time} seconds.")
-
-    # Save the fixed points
-    fixed_point_path = get_fixed_point_path(model_name, interpolated_input) 
-    torch.save(fixed_points, fixed_point_path)
-
-    print(f"Fixed points saved at {fixed_point_path}\n")
-
-# %%
