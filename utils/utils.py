eaa07717562b23e19de84d42524561369e0389c9
Sophie Libkind <4992461+slibkind@users.noreply.github.com>
2023-07-27 10:40:16 -0700

    FIX: latest kwarg to get model and get fixed point functions

--- utils/utils.py
+++ utils/utils.py
@@ -24,13 +24,17 @@ def get_metrics_path(model_name):
     model_dir = get_model_dir(model_name)
     return os.path.join(model_dir, "performance.json")
 
-def get_model_path(model_name, epoch=None):
+
+def get_model_path(model_name, epoch=None, latest=False):
     model_dir = get_model_dir(model_name)
     model_checkpoint_dir = os.path.join(model_dir, "model_checkpoint")
     
     # Check if the directory exists and create it if it doesn't
     if not os.path.isdir(model_checkpoint_dir):
         os.makedirs(model_checkpoint_dir)
+    
+    if latest:  # Get the latest model
+        return os.path.join(model_checkpoint_dir, "model_latest.pt")
 
     if epoch:  # Get the model of a specific epoch
         return os.path.join(model_checkpoint_dir, f"model_epoch_{epoch}.pt")
@@ -38,25 +42,25 @@ def get_model_path(model_name, epoch=None):
     return os.path.join(model_checkpoint_dir, "model.pt")
 
 
-def get_model_checkpoint(model_name, epoch=None):
-    model_path = get_model_path(model_name, epoch=epoch)
+def get_model_checkpoint(model_name, epoch=None, latest=False):
+    model_path = get_model_path(model_name, epoch=epoch, latest=latest)
     return torch.load(model_path)
 
-def load_checkpoint(model_name, model, optimizer, epoch=None):
+def load_checkpoint(model_name, model, optimizer, epoch=None, latest=False):
     """Load a model and optimizer state from a checkpoint file."""
-    checkpoint = get_model_checkpoint(model_name, epoch=epoch)
+    checkpoint = get_model_checkpoint(model_name, epoch=epoch, latest=latest)
     model.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     start_epoch = checkpoint['epoch']
     
     return model, optimizer, start_epoch
 
-def get_tasks(model_name, epoch=None):
-    model_checkpoint = get_model_checkpoint(model_name, epoch=epoch)
+def get_tasks(model_name, epoch=None, latest=False):
+    model_checkpoint = get_model_checkpoint(model_name, epoch=epoch, latest=latest)
     return model_checkpoint['tasks']
 
-def get_model(model_name, epoch=None):
-    model_checkpoint = get_model_checkpoint(model_name, epoch=epoch)
+def get_model(model_name, epoch=None, latest=False):
+    model_checkpoint = get_model_checkpoint(model_name, epoch=epoch, latest=latest)
     model_state_dict = model_checkpoint['model_state_dict']
     tasks = model_checkpoint['tasks']
 
@@ -70,8 +74,8 @@ def get_model(model_name, epoch=None):
     rnn.load_state_dict(model_state_dict)
     return rnn, tasks
 
-def get_model_epoch(model_name, epoch = None): 
-    checkpoint = get_model_checkpoint(model_name, epoch=epoch)
+def get_model_epoch(model_name, epoch=None, latest=False): 
+    checkpoint = get_model_checkpoint(model_name, epoch=epoch, latest=latest)
     return checkpoint['epoch']
 
 def get_analysis_path(model_name):
@@ -84,7 +88,7 @@ def get_analysis_path(model_name):
 
     return analysis_path
 
-def get_fixed_point_path(model_name, epoch = None):
+def get_fixed_point_path(model_name, epoch=None):
     analysis_path = get_analysis_path(model_name)
     fixed_point_path = os.path.join(analysis_path, "fixed_points")
 
